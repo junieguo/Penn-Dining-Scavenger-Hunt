fa4e2c046dcfadc276406d0b7b644a187a483f34
junieguo <junieguo@sas.upenn.edu>
2025-03-21 23:46:29 -0400

    Dining hall methods

--- Penn Dining Scavenger Hunt/Penn Dining Scavenger Hunt/ViewModels/DiningHallViewModel.swift
+++ Penn Dining Scavenger Hunt/Penn Dining Scavenger Hunt/ViewModels/DiningHallViewModel.swift
@@ -3,3 +3,28 @@
 //  Penn Dining Scavenger Hunt
 //
 
+import Foundation
+import CoreLocation
+
+@Observable class DiningHallViewModel: ObservableObject {
+    @Published var diningHalls: [DiningHall] = []
+    
+    // Method to mark a dining hall as collected
+    func collectDiningHall(diningHall: DiningHall) {
+        if let index = diningHalls.firstIndex(where: { $0.id == diningHall.id }) {
+            diningHalls[index].isCollected = true
+        }
+    }
+
+    // Method to check if a dining hall has been collected
+    func isDiningHallCollected(diningHall: DiningHall) -> Bool {
+        return diningHall.isCollected
+    }
+    
+    // Initialize with a list of dining halls
+    init() {
+        self.diningHalls = [
+            DiningHall(name: "Example", location: CLLocation(latitude: 0, longitude: 0)),
+        ]
+    }
+}
