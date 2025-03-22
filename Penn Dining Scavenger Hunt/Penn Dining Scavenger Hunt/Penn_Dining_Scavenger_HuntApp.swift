dcb64a21fc33b0ab82edf3967f7b7a98a612558d
Junie Guo <151979734+junieguo@users.noreply.github.com>
2025-03-21 23:31:49 -0400

    Project initialization

---
+++ Penn Dining Scavenger Hunt/Penn Dining Scavenger Hunt/Penn_Dining_Scavenger_HuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  Penn_Dining_Scavenger_HuntApp.swift
+//  Penn Dining Scavenger Hunt
+//
+//  Created by Junie Guo on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Penn_Dining_Scavenger_HuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
