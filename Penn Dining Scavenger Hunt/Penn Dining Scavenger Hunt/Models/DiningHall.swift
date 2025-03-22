1599f4befcc13d3f24e471b2b86ae4adfc3ef21e
junieguo <junieguo@sas.upenn.edu>
2025-03-21 23:42:47 -0400

    Dining hall model

--- Penn Dining Scavenger Hunt/Penn Dining Scavenger Hunt/Models/DiningHall.swift
+++ Penn Dining Scavenger Hunt/Penn Dining Scavenger Hunt/Models/DiningHall.swift
@@ -3,3 +3,11 @@
 //  Penn Dining Scavenger Hunt
 //
 
+import CoreLocation
+
+struct DiningHall: Identifiable {
+    var id: UUID = UUID()
+    var name: String
+    var location: CLLocation
+    var isCollected: Bool = false
+}
